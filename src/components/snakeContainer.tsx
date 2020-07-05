5668b20b67f493220345bfa5a23f82ab8845972c
Aek <ekkawin.v@gmail.com>
2020-07-06 01:09:46 +0700

    for got to commit

--- src/components/snakeContainer.tsx
+++ src/components/snakeContainer.tsx
@@ -1,3 +1,5 @@
 export const SnakeContainer = (prop) => (
-  <div className="w-full h-full flex align-center justify-center" {...prop} />
+  <div className="w-full h-full flex align-center justify-center">
+    {prop.children}
+  </div>
 );
